7eee46eb4793a7a38f03e9d60f4a9f1a013afdb3
eukamys <kamilly.yasmindepaula@gmail.com>
2025-09-04 16:43:34 -0300

    nove versão

---
+++ segundo programa.py
@@ -0,0 +1,35 @@
+#print("kamilly"*8000)
+#exemplo de variavel variavel do tipo texto 
+fruta ="manga"
+print(type(fruta))
+#exemplo váriavel do tipo INT é inteiro, números inteiros
+idade = int(35)
+#exemplo de números quebrados, float
+nota = float(9.5)
+
+#booleana
+vc_curte = false 
+
+### EXERCICIO 1
+
+# criar uma variavel que peça o nome do úsuario.
+# depois ciar uma váriavel de tipo float, que pede a nota do úsuario
+# depois criar uma váriavel que peça a idade do úsuario
+# depois de um print nas suas variaveis.
+
+#exemplo de variavel tipo texto
+nome = "kamilly"
+print(type(nome))
+
+#exemplo de números quebrados, float
+nota = float(10.5)
+
+#exemplo váriavel do tipo INT é inteiro, números inteiros 
+idade =int(15)
+
+nome_carro = ("ferrari")
+print(nome)
+preço = float(1000.0000)
+print(preço)
+ano = int(1929)
+print(ano)
